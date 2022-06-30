a7420c09525e605962c9bb82c983c7f0e1c7a2d2
maxluna12 <78857305+maxluna12@users.noreply.github.com>
2022-06-30 08:26:31 -0300

    Update Ejercicio_1.py

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,2 +1,7 @@
-Ejercicio 1
-Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!
+#Ejercicio 1
+#Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!
+#Se utiliza la función 'print'(imprimir) para mostrar la cadena por pantalla
+
+print("¡Hola Mundo!")
+#salida ¡Hola mundo!
+
